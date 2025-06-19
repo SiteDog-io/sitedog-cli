f3a033864a3406ed8e9432b93ac08c04f5505fb2
Kirill Bobykin <qelphybox@gmail.com>
2025-06-19 15:41:01 +0300

    bump version to v0.1.6

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	globalTemplatePath = ".sitedog/demo.html.tpl"
 	authFilePath       = ".sitedog/auth"
 	apiBaseURL         = "http://localhost:4567" // Change to your actual API URL
-	Version            = "v0.1.5"
+	Version            = "v0.1.6"
 	exampleConfig      = `# Describe your project with a free key-value format, think simple.
 #
 # Random sample:
